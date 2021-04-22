e6c2b5a19711e9452654e6fec30dbd5ee2c84b8a
tomaswyz <jackywyz@126.com>
2021-04-22 12:11:14 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+curl -fsSL -o podman-linux-amd64.tar.gz https://download.fastgit.org/mgoltzsche/podman-static/releases/latest/download/podman-linux-amd64.tar.gz
+tar -xzf podman-linux-amd64.tar.gz
+sudo cp -r podman-linux-amd64/usr podman-linux-amd64/etc /
+
+
+sudo sh -c "echo $(id -un):100000:200000 >> /etc/subuid"
+sudo sh -c "echo $(id -gn):100000:200000 >> /etc/subgid"
