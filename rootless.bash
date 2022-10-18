ee306d02d17f65ba1875fe891991fd9ca837081f
tomasky <blueswooz@outlook.com>
2022-10-18 10:38:52 +0800

    feat: update rootless bash

--- rootless.bash
+++ rootless.bash
@@ -8,12 +8,15 @@ $sudo sysctl -w "net.ipv4.ping_group_range=0 65536"
 uname=$(id -un)
 gname=$(id -gn)
 HOME=$HOME
+execommand=bash
+
 if [[ "$uname" = "root" ]]; then
   HOME="/home/podman"
   useradd --create-home --shell /bin/bash -d $HOME -u 1001 -m podman
   chown -R podman:podman $HOME 
   uname=podman
   gname=podman
+  execommand=$(su $uname)
 fi
 
 if ! grep -q "$uname:100000:65536" /etc/subuid; then
@@ -21,13 +24,12 @@ if ! grep -q "$uname:100000:65536" /etc/subuid; then
   $sudo sh -c "echo $gname:100000:65536 >> /etc/subgid"
 fi
 
-su $uname -c '
-mkdir -p $HOME/.local/
-mkdir -p $HOME/.local/podman && cp -r * $HOME/.local/podman
+$execommand -c '
+mkdir -p $HOME/.local/podman && \cp -rf * $HOME/.local/podman
 
 mkdir -p $HOME/.config && cp -r $HOME/.local/podman/etc/containers $HOME/.config/
 cd $HOME/.config/containers
-rm storage.conf && mv crun-containers.conf containers.conf
+mv storage.conf storage.conf.bak && mv crun-containers.conf containers.conf
 sed -i "s|\$HOME|$HOME|g" containers.conf
 
 if test -f $HOME/.bashrc && ! grep -q "export PATH=\"\$HOME/.local/podman" $HOME/.bashrc; then
@@ -35,6 +37,8 @@ if test -f $HOME/.bashrc && ! grep -q "export PATH=\"\$HOME/.local/podman" $HOME
 fi
 '
 
-if [[ "$uname" != "root" ]]; then
+if [[ "$(id -un)" = "root" ]]; then
   su $uname
+else
+  exec $SHELL -l
 fi
